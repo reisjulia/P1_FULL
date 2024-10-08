552a855f9554408969e44b223be1e5d6ec60d76c
Julia Reis <juliareisrodrigues5@gmail.com>
2024-10-07 22:22:37 -0300

    alteracao_user

---
+++ src/repositories/RoomRepository.js
@@ -0,0 +1,1 @@
+//criar o repository da room 
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -5,7 +5,7 @@ const auth = require('../middlewares/auth')
 const router = express.Router();
 
 router.post('/register', UserController.store); // p registro
-router.post('login', UserController.login); // p[ login
+router.post('login', UserController.login); // p login
 router.get('/users', UserController.index); // p listar todos
 router.get('users/:id', auth, UserController.show); // p Mostrar 
 router.put('users/:id', auth, UserController.update); // p lterar dados usuario
